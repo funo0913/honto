97ed6bf487783ac426450476756ca05a2bece6b6
funo0913 <funo0913@gmail.com>
2018-05-27 17:47:26 +0900

    退会処理実装(物理削除)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   def top
+    # 残っているメッセージは削除する
+    if flash[:alert].present?
+      flash.delete(:alert)
+    end
     # LPの表示
     render layout: false
+  end
+  def show
+    @user = User.find(current_user.id)
+    @search_book = Book.new
   end
 end

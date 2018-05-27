cc0bda7d04b732d331d12bf4409e1bf9c010faa7
funo0913 <funo0913@gmail.com>
2018-05-27 15:27:19 +0900

    検索機能追加、全件取得未実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_brank_book, only:[:index, :index_my_bookshelf,:index_search_review,:show,:edit]
+  before_action :set_status, only:[:index_my_bookshelf,:edit]
 
   # 感想の新着一覧表示(最新２０件)
   def index
@@ -7,25 +9,23 @@ class ReviewsController < ApplicationController
     if !@reviews.nil?
       @reviews = Kaminari.paginate_array(@reviews).page(params[:page]).per(10)
     end
-    @search_book = Book.new
   end
 
   # 自分の本棚の一覧表示
   def index_my_bookshelf
-    @reviews = Review.where(user_id: current_user.id).page(params[:page]).per(10)
-    @search_book = Book.new
+    # @reviews = Review.where(user_id: current_user.id).page(params[:page]).per(10)
+    @q = Review.search(search_params)
+    @reviews = @q.result.where(user_id: current_user.id).page(params[:page]).per(10)
   end
 
   # 書籍検索結果からの感想一覧表示
   def index_search_review
     book = Book.find_by(tmp_id: params[:id])
     @reviews = Review.where(book_id: book.id).where(status_id: 3).page(params[:page]).per(10)
-    @search_book = Book.new
   end
 
   # 感想の詳細表示
   def show
-    @review = Review.find(params[:id])
     # 自分の感想か、他人の感想か？
     if @review.user_id == current_user.id
       #自分の感想
@@ -44,7 +44,6 @@ class ReviewsController < ApplicationController
         end
       end
     end
-    @search_book = Book.new
     @statuses = Status.all
   end
 
@@ -55,10 +54,7 @@ class ReviewsController < ApplicationController
 
   # 感想の編集・更新
   def edit
-    @search_book = Book.new
-    @review = Review.find(params[:id])
     @book = Book.find(@review.book_id)
-    @statuses = Status.all
   end
 
 
@@ -101,7 +97,12 @@ class ReviewsController < ApplicationController
     def set_review
       @review = Review.find(params[:id])
     end
-
+    def set_brank_book
+      @search_book = Book.new
+    end
+    def set_status
+      @statuses = Status.all
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
       params.require(:review).permit(:user_id,
@@ -114,4 +115,9 @@ class ReviewsController < ApplicationController
                                      :warning
                                    )
     end
+    def search_params
+      debugger
+      params.require(:q).permit(:status_id_eq)
+    end
+
 end

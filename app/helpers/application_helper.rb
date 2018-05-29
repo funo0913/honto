492a251b511c2d1d3dc256b0a6622989f457ba16
funo0913 <funo0913@gmail.com>
2018-05-29 19:38:47 +0900

    本棚の冊数表示処理をヘルパーに切り出し

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,31 @@ module ApplicationHelper
     end
     html.html_safe
   end
+
+  # ヘッダーに表示する本の冊数アイコン
+  def mybookshelf_books(count)
+    html = ""
+    if count == 1
+      html += <<-EOF
+        <img height="18" src="/assets/last-book.png" class="last-book" />
+      EOF
+    elsif !count.nil? && count > 1
+      for i in 1..count do
+        if i == count
+          html += <<-EOF
+          <img height="18" src="/assets/last-book.png" class="last-book"/>
+          EOF
+        elsif i == 1
+          html += <<-EOF
+          <img height="18" src="/assets/first-book.png" class="first-book"/>
+          EOF
+        else
+          html += <<-EOF
+          <img height="18" src="/assets/first-book.png" class="first-book"/>
+          EOF
+        end
+      end
+    end
+    html.html_safe
+  end
 end
